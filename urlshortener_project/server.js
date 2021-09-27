16ae095fa02d2ad71b82c1ff04f46c10dbb3f227
Hugo Venega <hugo.venega.00@gmail.com>
2021-09-27 15:30:21 -0300

    Add app.get check the short_url and redirect to the assigned url

--- urlshortener_project/server.js
+++ urlshortener_project/server.js
@@ -101,7 +101,28 @@ app.post('/api/shorturl', (req, res) => {
     }); 
   }); 
 
-
+  app.get('/api/shorturl/:shorturl', (req, res) => {
+    const { shorturl } = req.params;
+    //it takes the short_url and if it finds it in the database it redirects
+    Url.findOne({
+      short_url: shorturl
+    }, (err, urlFound) => {
+      if (err) {
+        console.log('findOne() error');
+      }
+     
+      if (!urlFound) {
+        res.json({
+          error: 'No short URL found for the given input'
+        });
+      } else {
+        
+        res.redirect(urlFound.original_url);
+      }
+    }); 
+  }); 
+  
+  
 
 
 app.listen(port);
