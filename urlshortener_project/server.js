09fbac4a88d6e291a55b21ea09413972e13cbac5
Hugo Venega <hugo.venega.00@gmail.com>
2021-09-27 15:26:56 -0300

    Add app.post to check and save url to database

--- urlshortener_project/server.js
+++ urlshortener_project/server.js
@@ -41,7 +41,65 @@ app.get("/", function (req, res) {
 });
 app.use('/public',express.static(__dirname + '/public'));
 
-
+app.post('/api/shorturl', (req, res) => {
+  // save url from input
+    const urlRequest = req.body.url;
+  // leave only the hostname  www.example.com
+    const hostname = urlRequest
+      .replace(/http[s]?\:\/\//, '')
+      .replace(/\/(.+)?/, '');
+      //does the host exist?
+    dns.lookup(hostname, (lookupErr, addresses) => {
+      if (lookupErr) {
+        console.log('lookup() error');
+      }
+      if (!addresses) {
+        res.json({
+          error: 'invalid URL'
+        });
+      } else {
+  
+        //if the url it exists it searches the database if it is already entered
+        Url.findOne({
+          original_url: urlRequest
+        }, (findOneErr, urlFound) => {
+          if (findOneErr) {
+            console.log('findOne() error');
+          }
+          // if you can't find the url, register it in the database
+          //count how many documents are stored in the database
+          if (!urlFound) {
+            Url.estimatedDocumentCount((countErr, count) => {
+              if (countErr) {
+                res.send('estimatedDocumentCount() error');
+              }
+              // ccreate the new document and the short_url assigns it a number according to the one that corresponds
+              const url = new Url({
+                original_url: urlRequest,
+                short_url: count + 1
+              });
+              //save the document in the database
+              url.save((saveErr, urlSaved) => {
+                if (saveErr) {
+                  res.send('save() error');
+                }
+                res.json({
+                  original_url: urlSaved.original_url,
+                  short_url: urlSaved.short_url
+                });
+              });
+            });        
+          } else {
+            
+            res.json({
+              original_url: urlFound.original_url,
+              short_url: urlFound.short_url
+            });
+          } 
+        }); 
+      } 
+    }); 
+  }); 
 
 
 
