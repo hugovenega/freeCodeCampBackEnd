ceeffd77c093d9303e2486b6cd0a70a60f9a2c31
Hugo Venega <hugo.venega.00@gmail.com>
2021-09-28 18:50:19 -0300

    Fix the code with eslint

--- filemetadata_project/server.js
+++ filemetadata_project/server.js
@@ -1,27 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-var multer= require('multer');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
 app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
-  
   res.json({
-  name : req.file.originalname,
-  type : req.file.mimetype,
-  size : req.file.size,
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
   });
 });
 
-
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
