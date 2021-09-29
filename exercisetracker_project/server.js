4bfde9165bd2f073947469c1442f61c08b845ce7
Hugo Venega <hugo.venega.00@gmail.com>
2021-09-29 15:48:47 -0300

    Starting code

--- exercisetracker_project/server.js
+++ exercisetracker_project/server.js
@@ -16,3 +16,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
