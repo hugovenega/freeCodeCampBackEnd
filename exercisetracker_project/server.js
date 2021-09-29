b11580f9e128467aed5b63977719ed35173dcc3d
Hugo Venega <hugo.venega.00@gmail.com>
2021-09-29 16:00:17 -0300

    Add post for add new exercise

--- exercisetracker_project/server.js
+++ exercisetracker_project/server.js
@@ -69,3 +69,54 @@ app.post('/api/users', async (req, res) => {
 app.get('/api/users', (req, res) => {
   res.send([...users]); // SEND ARRAY WITH USERS ARRAY
 });
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  // TYPEDID SHOULD BE = TO WHAT IS TYPED IN URL OR USER ID
+  const typedId = req.body.userId || req.params._id;
+  // GET WHAT TYPED IN DESCRIPTION FIELD
+  const typedDes = req.body.description;
+  // GET WHAT TYPED IN DURATION FIELD
+  const typedDur = parseInt(req.body.duration);
+  // CREATE A VAR TYPEDDATE TO STORE DATE LATER
+  let typedDate;
+
+  try {
+    // CHECK FOR EXISTING USER WITH ID MATCHING TYPEDID
+    const foundUser = await User.findOne({ _id: typedId });
+    if (foundUser) { // YES => IF THE DATE WAS TYPED
+      if (new Date(req.body.date) == 'Invalid Date') {
+        // NO, SET TYPEDDATE TO TODAYS DATE
+        typedDate = new Date().toDateString();
+      } else {
+      // YES, STORE THAT DATE IN TYPEDDATE
+        typedDate = new Date(req.body.date).toDateString();
+      }
+      // --------------------------------------
+
+      // CREATE NEW EXERCISE & SAVE
+      const newExe = new Exe({
+        key: foundUser._id,
+        date: typedDate,
+        duration: typedDur,
+        description: typedDes,
+      });
+      newExe.save();
+      // --------------------------------------
+
+      // SEND RESPONCE
+      res.json({
+        _id: foundUser._id,
+        username: foundUser.username,
+        date: typedDate,
+        duration: typedDur,
+        description: typedDes,
+      });
+    }
+    // --------------------------------------
+
+    // CHECK FOR ERRORS
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Server Error');
+  }
+});
