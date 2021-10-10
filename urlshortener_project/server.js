2298bb9e6035a0a07bf7a2def4de2a7eb381e212
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-10 13:15:55 -0300

    reorganize requires, uses and rename constants

--- urlshortener_project/server.js
+++ urlshortener_project/server.js
@@ -1,23 +1,23 @@
-const port = 3000;
-
-// all necesary requires
 const express = require('express');
-require('dotenv').config();
+const dotEnv = require('dotenv');
 const dns = require('dns');
-const mongodb = require('mongodb');
+const createCorsMiddleware = require('cors');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const app = express();
-app.use(cors({ optionsSuccessStatus: 200 }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const port = 3000;
+const corsMiddlewareOpts = { optionsSuccessStatus: 200 };
+const urlencodedOpts = { extended: true };
+dotEnv.config();
 
-// server config
+app.use(createCorsMiddleware(corsMiddlewareOpts));
+app.use(express.urlencoded(urlencodedOpts));
+app.use(express.json());
+app.use('/public', express.static(`${__dirname}/public`));
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
 });
-// URL object config
 const { Schema } = mongoose;
 const urlSchema = new Schema({
   original_url: {
@@ -35,16 +35,11 @@ const Url = mongoose.model('Url', urlSchema);
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
-app.use('/public', express.static(`${__dirname}/public`));
-
 app.post('/api/shorturl', (req, res) => {
-  // save url from input
   const urlRequest = req.body.url;
-  // leave only the hostname  www.example.com
   const hostname = urlRequest
     .replace(/http[s]?\:\/\//, '')
     .replace(/\/(.+)?/, '');
-  // does the host exist?
   dns.lookup(hostname, (lookupErr, addresses) => {
     if (lookupErr) {
       console.log('lookup() error');
@@ -54,27 +49,21 @@ app.post('/api/shorturl', (req, res) => {
         error: 'invalid URL',
       });
     } else {
-      // if the url it exists it searches the database if it is already entered
       Url.findOne({
         original_url: urlRequest,
       }, (findOneErr, urlFound) => {
         if (findOneErr) {
           console.log('findOne() error');
         }
-        // if you can't find the url, register it in the database
-        // count how many documents are stored in the database
         if (!urlFound) {
           Url.estimatedDocumentCount((countErr, count) => {
             if (countErr) {
               res.send('estimatedDocumentCount() error');
             }
-            // ccreate the new document and the short_url assigns it a
-            // number according to the one that corresponds
             const url = new Url({
               original_url: urlRequest,
               short_url: count + 1,
             });
-              // save the document in the database
             url.save((saveErr, urlSaved) => {
               if (saveErr) {
                 res.send('save() error');
@@ -95,17 +84,14 @@ app.post('/api/shorturl', (req, res) => {
     }
   });
 });
-
 app.get('/api/shorturl/:shorturl', (req, res) => {
   const { shorturl } = req.params;
-  // it takes the short_url and if it finds it in the database it redirects
   Url.findOne({
     short_url: shorturl,
   }, (err, urlFound) => {
     if (err) {
       console.log('findOne() error');
     }
-
     if (!urlFound) {
       res.json({
         error: 'No short URL found for the given input',
