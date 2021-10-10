3bdfb3266fa320c56e76188fe6cf505708b043bb
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-10 13:02:27 -0300

    Modify the methods with the appropriate handler

--- timestamp_project/server.js
+++ timestamp_project/server.js
@@ -10,32 +10,9 @@ const app = express();
 app.use(createCorsMiddleware(corsMiddlewareOpts));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
-});
-
-app.get('/api/:date', (req, res) => {
-  let timeStamp = req.params.date;
-  if (timeStamp.match(/\d{5,}/)) {
-    timeStamp = +timeStamp;
-  }
-  const date = new Date(timeStamp);
-  if (date.toUTCString() === 'Invalid Date') {
-    res.json({ error: 'Invalid Date' });
-  }
-  res.json({
-    unix: date.valueOf(),
-    utc: date.toUTCString(),
-  });
-});
-
-app.get('/api', (req, res) => {
-  const date = new Date();
-  res.json({
-    unix: date.valueOf(),
-    utc: date.toUTCString(),
-  });
-});
+app.get('/', serverIndexController);
+app.get('/api/:date', dateEntryController);
+app.get('/api', dateNowController);
 
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
