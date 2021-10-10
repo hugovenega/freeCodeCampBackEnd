b15e35aed285b4d0da8a4644eb963770d3b1cd72
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-10 12:51:17 -0300

    Delete unnecessary comments

--- timestamp_project/server.js
+++ timestamp_project/server.js
@@ -1,26 +1,14 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
-
 const app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 const cors = require('cors');
 
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
-// your first API endpoint...
 app.get('/api/:date', (req, res) => {
   let timeStamp = req.params.date;
   if (timeStamp.match(/\d{5,}/)) {
@@ -44,7 +32,6 @@ app.get('/api', (req, res) => {
   });
 });
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
