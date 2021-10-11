ff10be1cfe6e87f341edd8d4e6de5c28b2fe613b
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-11 18:49:54 -0300

    Modify by ordering requires, constants and removing comments

--- headerparser_project/server.js
+++ headerparser_project/server.js
@@ -1,32 +1,20 @@
-// server.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 const express = require('express');
+const createCorsMiddleware = require('cors');
 
 const app = express();
+const corsMiddlewareOpts = { optionsSuccessStatus: 200 };
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-const cors = require('cors');
-
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+app.use(createCorsMiddleware(corsMiddlewareOpts));
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
-
-// your first API endpoint...
 app.get('/api/whoami', (req, res) => {
   res.json({ ipaddress: req.connection.remoteAddress, language: req.headers['accept-language'], software: req.headers['user-agent'] });
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
